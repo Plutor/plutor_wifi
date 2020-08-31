5e5bc69293be140db687b813e77afa26b15b46d3
Logan Ingalls <lingalls@gmail.com>
2020-08-31 09:41:17 -0400

    Refactor into a class so we can have a single db object

--- run.py
+++ run.py
@@ -24,169 +24,172 @@ def parseargs():
     parser.add_argument('--skip_test', action='store_true')
     return parser.parse_args()
 
-def run_speedtests():
-    """Runs all configured speedtests.
+class PlutorWifi(object):
 
-    returns:
-        a hash of speedtest names to (down Mbps, up Mbps, ping Mbps) data
-    """
-    rv = {}
-    speedtest = run_ookla()
-    if speedtest:
-        rv['speedtest'] = speedtest
-    fastcom = run_fastcom()
-    if fastcom:
-        rv['fastcom'] = fastcom
-    mlab = run_mlabndt()
-    if mlab:
-        rv['mlab'] = mlab
-    return rv
+    def __init__(self):
+        # Load config
+        with open(CFG) as f:
+            data = f.read()
+            self.cfg = json.loads(data)
 
-def run_ookla():
-    """Runs Ookla speedtest.net
+        # Load recent history
+        self.db = tinydb.TinyDB(TINYDB)
+        Rec = tinydb.Query()
+        self.hist = self.db.search(Rec.timestamp > NOW - 24*60*60)
 
-    returns:
-        (down Mbps, up Mbps, ping Mbps)
-    """
-    print("Running ookla speedtest.net")
-    cp = subprocess.run(['speedtest-cli', '--simple'], stdout=subprocess.PIPE)
-    print(cp)
-    if cp.returncode != 0:
-        print('Bad return code %d' % cp.returncode)
-        return None
-    down = up = ping = -1
-    for line in cp.stdout.split(b"\n"):
-        try:
-            type, val, unit = line.split()
-            if type == b'Download:':
-                down = float(val)
-            elif type == b'Upload:':
-                up = float(val)
-            elif type == b'Ping:':
-                ping = float(val)
-        except:
-            next
-    return (down, up, ping)
+    def run_speedtests(self):
+        """Runs all configured speedtests.
 
-def run_fastcom():
-    """Runs Fast.com test
+        returns:
+            a hash of speedtest names to (down Mbps, up Mbps, ping Mbps) data
+        """
+        rv = {}
+        speedtest = self.run_ookla()
+        if speedtest:
+            rv['speedtest'] = speedtest
+        fastcom = self.run_fastcom()
+        if fastcom:
+            rv['fastcom'] = fastcom
+        mlab = self.run_mlabndt()
+        if mlab:
+            rv['mlab'] = mlab
+        return rv
 
-    returns:
-        (down Mbps, None, None)
-    """
-    print("Running fast.com")
-    cp = subprocess.run(['fast-cli', '-s'], stdout=subprocess.PIPE)
-    print(cp)
-    if cp.returncode != 0:
-        print('Bad return code %d' % cp.returncode)
-        return None
-    val, unit = cp.stdout.split()
-    return (float(val), None, None)
+    def run_ookla(self):
+        """Runs Ookla speedtest.net
 
-def run_mlabndt():
-    """Runs M-Lab NDT test
+        returns:
+            (down Mbps, up Mbps, ping Mbps)
+        """
+        print("Running ookla speedtest.net")
+        cp = subprocess.run(['speedtest-cli', '--simple'], stdout=subprocess.PIPE)
+        print(cp)
+        if cp.returncode != 0:
+            print('Bad return code %d' % cp.returncode)
+            return None
+        down = up = ping = -1
+        for line in cp.stdout.split(b"\n"):
+            try:
+                type, val, unit = line.split()
+                if type == b'Download:':
+                    down = float(val)
+                elif type == b'Upload:':
+                    up = float(val)
+                elif type == b'Ping:':
+                    ping = float(val)
+            except:
+                next
+        return (down, up, ping)
 
-    returns:
-        (down Mbps, up Mbps, ping Mbps)
-    """
-    print("Running M-Lab NDT")
-    cp = subprocess.run(['ndt7-client', '-quiet', '-format=json'], stdout=subprocess.PIPE)
-    print(cp)
-    if cp.returncode != 0:
-        print('Bad return code ', cp.returncode)
-        return None
-    data = json.loads(cp.stdout.decode("utf-8")) 
-    return (data['Download']['Value'], data['Upload']['Value'], data['MinRTT']['Value'])
+    def run_fastcom(self):
+        """Runs Fast.com test
+
+        returns:
+            (down Mbps, None, None)
+        """
+        print("Running fast.com")
+        cp = subprocess.run(['fast-cli', '-s'], stdout=subprocess.PIPE)
+        print(cp)
+        if cp.returncode != 0:
+            print('Bad return code %d' % cp.returncode)
+            return None
+        val, unit = cp.stdout.split()
+        return (float(val), None, None)
+
+    def run_mlabndt(self):
+        """Runs M-Lab NDT test
+
+        returns:
+            (down Mbps, up Mbps, ping Mbps)
+        """
+        print("Running M-Lab NDT")
+        cp = subprocess.run(['ndt7-client', '-quiet', '-format=json'], stdout=subprocess.PIPE)
+        print(cp)
+        if cp.returncode != 0:
+            print('Bad return code ', cp.returncode)
+            return None
+        data = json.loads(cp.stdout.decode("utf-8")) 
+        return (data['Download']['Value'], data['Upload']['Value'], data['MinRTT']['Value'])
 
 
-# ========================
+    # ========================
 
-def save_data(data, do_tweet):
-    db = tinydb.TinyDB(TINYDB)
-    db.insert({'tweeted': do_tweet, 'data': data, 'timestamp': NOW})
+    def save_data(self, data, do_tweet):
+        row = {'data': data, 'tweeted': do_tweet, 'timestamp': NOW}
+        self.db.insert(row)
+        self.hist.append(row)
 
-def get_history(max_age_secs):
-    db = tinydb.TinyDB(TINYDB)
-    Rec = tinydb.Query()
-    return db.search(Rec.timestamp > NOW - max_age_secs)
+    def generate_graph(self):
+        """Returns med_down, med_up."""
+        print("Creating graph")
+        # Define data
+        x = []
+        ys = [[], [], [], [], []]
+        for run in self.hist:
+            x.append(datetime.datetime.fromtimestamp(run['timestamp']))
+            if 'speedtest' in run['data']:
+                ys[0].append(run['data']['speedtest'][0])
+                ys[3].append(run['data']['speedtest'][1])
+            else:
+                ys[0].append(None)
+                ys[3].append(None)
+            if 'fastcom' in run['data']:
+                ys[1].append(run['data']['fastcom'][0])
+            else:
+                ys[1].append(None)
+            if 'mlab' in run['data']:
+                ys[2].append(run['data']['mlab'][0])
+                ys[4].append(run['data']['mlab'][1])
+            else:
+                ys[2].append(None)
+                ys[4].append(None)
 
-def generate_graph(hist):
-    """Returns med_down, med_up."""
-    print("Creating graph")
-    # Define data
-    x = []
-    ys = [[], [], [], [], []]
-    for run in hist:
-        x.append(datetime.datetime.fromtimestamp(run['timestamp']))
-        if 'speedtest' in run['data']:
-            ys[0].append(run['data']['speedtest'][0])
-            ys[3].append(run['data']['speedtest'][1])
-        else:
-            ys[0].append(None)
-            ys[3].append(None)
-        if 'fastcom' in run['data']:
-            ys[1].append(run['data']['fastcom'][0])
-        else:
-            ys[1].append(None)
-        if 'mlab' in run['data']:
-            ys[2].append(run['data']['mlab'][0])
-            ys[4].append(run['data']['mlab'][1])
-        else:
-            ys[2].append(None)
-            ys[4].append(None)
+        med_down = statistics.median(filter(None, ys[0]+ys[1]+ys[2]))
+        med_up = statistics.median(filter(None, ys[3]+ys[4]))
 
-    med_down = statistics.median(filter(None, ys[0]+ys[1]+ys[2]))
-    med_up = statistics.median(filter(None, ys[3]+ys[4]))
+        # Plot data including options
+        fig, ax = plt.subplots(figsize=(8, 4.4))
+        for y in ys:
+            ax.plot(x, y)
 
-    # Plot data including options
-    fig, ax = plt.subplots(figsize=(8, 4.4))
-    for y in ys:
-        ax.plot(x, y)
+        # Add plot details
+        plt.ylabel('Mbps')
+        plt.legend(['speedtest down', 'fast.com down', 'mlab down', 'speedtest up', 'mlab up'])
+        plt.style.use('fivethirtyeight')
 
-    # Add plot details
-    plt.ylabel('Mbps')
-    plt.legend(['speedtest down', 'fast.com down', 'mlab down', 'speedtest up', 'mlab up'])
-    plt.style.use('fivethirtyeight')
+        # Save the plot
+        plt.savefig(PLOTPNG, dpi=100, bbox_inches='tight')
+        plt.show()
 
-    # Save the plot
-    plt.savefig(PLOTPNG, dpi=100, bbox_inches='tight')
-    plt.show()
+        return med_down, med_up
 
-    return med_down, med_up
+    def tweet_history(self, max_age_secs):
+        auth = tweepy.OAuthHandler(self.cfg['api_key'], self.cfg['api_secret'])
+        auth.set_access_token(self.cfg['access_token'], self.cfg['access_token_secret'])
+        api = tweepy.API(auth)
+        med_down, med_up = self.generate_graph()
+        print("Tweeting graph")
+        media = api.media_upload(PLOTPNG)
+        api.update_status('Median speed: %.1f Mbps down / %.1f Mbps up' % (med_down, med_up), media_ids=[media.media_id])
 
-def load_config():
-    with open(CFG) as f:
-        data = f.read()
-        return json.loads(data)
-
-def tweet_history(max_age_secs):
-    hist = get_history(max_age_secs)
-    cfg = load_config()
-    auth = tweepy.OAuthHandler(cfg['api_key'], cfg['api_secret'])
-    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
-    api = tweepy.API(auth)
-    med_down, med_up = generate_graph(hist)
-    print("Tweeting graph")
-    media = api.media_upload(PLOTPNG)
-    api.update_status('Median speed: %.1f Mbps down / %.1f Mbps up' % (med_down, med_up), media_ids=[media.media_id])
-
-def tweet_due(max_age_secs):
-    for rec in get_history(max_age_secs):
-        if rec['tweeted']:
-            return False
-    return True
+    def tweet_due(self, max_age_secs):
+        for rec in self.hist:
+            if rec['tweeted'] and rec['timestamp'] > NOW - max_age_secs:
+                return False
+        return True
 
 def main():
     args = parseargs()
-    do_tweet = (args.force_tweet or tweet_due(8*60*60)) and not args.only_test
+    p = PlutorWifi()
 
+    do_tweet = (args.force_tweet or p.tweet_due(8*60*60)) and not args.only_test
     if not args.skip_test:
-        data = run_speedtests()
+        data = p.run_speedtests()
         if data:
-            save_data(data, do_tweet)
+            p.save_data(data, do_tweet)
         
     if do_tweet:
-        tweet_history(24*60*60)
+        p.tweet_history(24*60*60)
     else:
         print("Skipping tweet")
 
